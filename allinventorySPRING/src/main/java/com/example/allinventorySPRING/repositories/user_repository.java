a8c2dfa251eaf0d36869c4245fa3b0c23fdd636e
darlynmillan <116969471+darlynmillan@users.noreply.github.com>
2022-10-30 18:43:43 -0500

    Delete allinventorySPRING directory

--- allinventorySPRING/src/main/java/com/example/allinventorySPRING/repositories/user_repository.java
+++
@@ -1,8 +0,0 @@
-package com.example.allinventorySPRING.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.allinventorySPRING.models.user_model;
-
-public interface user_repository extends CrudRepository<user_model, Long> {
-
-}
